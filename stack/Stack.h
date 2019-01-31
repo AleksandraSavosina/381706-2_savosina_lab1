2f718cfbfec8e024543f5a012ba283bc4c017cbb
Elijah <Superil2q@yandex.ru>
2019-01-31 14:49:48 +0300

    stack refactor

--- stack/Stack.h
+++ stack/Stack.h
@@ -1,5 +1,5 @@
-
 #ifndef __STACK_H__
+
 #define __STACK_H__
 
 #include <iostream>
@@ -7,84 +7,96 @@
 using namespace std;
 
 template <class T>
+
 class TStack
 {
 protected:
-  int size;		// ðàçìåð ñòåêà
-  T* mas;			// ýëåìåíòû ñòåêà
-  int top;		// âåðõ
+	int size; // размер стека
+	T* mas; // элементы стека
+	int top; // верх
 public:
-  TStack()				// êîíñòðóêòîð ïî óìîë÷àíèþ
-  {
-    size = 0;
-    mas = 0;
-    top = 0;
-  }
-  TStack(int _size)		// êîíñòðóêòîð ñ ïàðàìåòðîì
-  {
-    if (_size < 0)
-      throw "negative size";
+	TStack() // конструктор по умолчанию
+	{
+		size = 0;
+		mas = 0;
+		top = 0;
+	}
+	TStack(int _size) // конструктор с параметром
+	{
+		if (_size < 0)
+			throw "negative size";
 
-    if (_size == 0)
-    {
-      size = 0;
-      mas = 0;
-      top = 0;
-    }
-    else
-    {
-      mas = new T[_size];
-      size = _size;
-      top = 0;
-    }
-  }
-  TStack(TStack &h)		// êîíñòðóêòîð êîïèðîâàíèÿ
-  {
-    size = h.size;
-    top = h.top;
-    if (size == 0)
-      mas = 0;
-    else
-    {
-      mas = new T[size];
-      for (int i = 0; i < size; i++)
-        mas[i] = h.mas[i];
-    }
-  }
+		if (_size == 0)
+		{
+			size = 0;
+			mas = 0;
+			top = 0;
+		}
 
-  ~TStack()				// äåñòðóêòîð
-  {
-    delete[] mas;
-  }
+		else
+		{
 
-  void Put(T h)			// ïîëîæèòü
-  {
-    if (IsFull())
-      throw "stack is full";
-    else
-    {
-      mas[top] = h;
-      top++;
-    }
-  }
-  T Get()				// âçÿòü
-  {
-    if (IsEmpty())
-      throw "stack is empty";
-    else
-    {
-      top--;
-      return mas[top];
-    }
-  }
-  bool IsFull()			// ïðîâåðêà íà ïîëíîòó
-  {
-    return (top >= size);
-  }
-  bool IsEmpty()			// ïðîâåðêà íà ïóñòîòó
-  {
-    return (top == 0);
-  }
+		    mas = new T[_size];
+			size = _size;
+			top = 0;
+		}
+	}
+
+	TStack(TStack &h) // конструктор копирования
+	{
+		size = h.size;
+		top = h.top;
+		if (size == 0)
+			mas = 0;
+
+		else
+		{
+			mas = new T[size];
+			for (int i = 0; i < size; i++)
+				mas[i] = h.mas[i];
+		}
+	}
+
+	~TStack() // деструктор
+	{
+		delete[] mas;
+	}
+
+	void Put(T h) // положить
+	{
+		if (IsFull())
+			throw "stack is full";
+
+		else
+		{
+			mas[top] = h;
+			top++;
+		}
+	}
+
+	T Get() // взять
+	{
+
+		if (IsEmpty())
+			throw "stack is empty";
+		else
+		{
+			--top;
+			return mas[top];
+		}
+
+	}
+
+	bool IsFull() // проверка на полноту
+	{
+		return (top >= size);
+	}
+
+	bool IsEmpty() // проверка на пустоту
+	{
+		return (top == 0);
+	}
+
 };
 
 #endif
--- stack/stack_main.cpp
+++ stack/stack_main.cpp
@@ -1,59 +1,55 @@
-#include "Stack.h"
+#include "stack.h"
 
 int main()
 {
-	
-  int n;
-  cout << "Enter size of stack: ";
-  cin >> n;
-
-
-  TStack<int> help(n);
-  int j = 0;
-  cout << "Put - 1 / Get - 2 / End - 0" << endl;
-  int elem;
-  cin >> j;
-  while (j != 0)
-  {
-    switch (j)
-    {
-      case 1: 
-	if (help.IsFull())
+	int n;
+	cout << "Enter size of stack: ";
+	cin >> n;
+	TStack<int> help(n);
+	int j = 0;
+	cout << "Put - 1 / Get - 2 / End - 0" << endl;
+	int elem;
+	cin >> j;
+	while (j != 0)
 	{
-	  cout << "Sorry. Stack is full" << endl;
-	  break;
-	}
-	cout << "Put: " << endl;
-	cin >> elem;
-	help.Put(elem);
-	break;
-      case 2:
-	if (help.IsEmpty())
-        {
-	  cout << "Sorry. Stack is empty" << endl;
-	  break;
-	}
-	cout << "Get: ";
-	cout << help.Get() << endl;
-	break;
-      default:
-	break;
-      }
-	
-      cout << "Enter your choice: ";
-      cin >> j;
-      if ((j < 0) || (j > 3))
-      {
-	cout << "!!! Incorrect value !!!" << endl;
-	break;
-      }
-    }
+		switch (j)
+		{
+		case 1:
+			if (help.IsFull())
+			{
+				cout << "Sorry. Stack is full" << endl;
+				break;
+			}
 
+			cout << "Put: " << endl;
+			cin >> elem;
+			help.Put(elem);
+			break;
+		case 2:
+			if (help.IsEmpty())
+			{
+				cout << "Sorry. Stack is empty" << endl;
+				break;
+			}
+
+			cout << "Get: ";
+			cout << help.Get() << endl;
+			break;
+		default:
+			break;
+		}
+
+		cout << "Enter your choice: ";
+		cin >> j;
+		if ((j < 0) || (j > 3))
+		{
+			cout << "!!! Incorrect value !!!" << endl;
+			break;
+		}
+	}
 	if (help.IsEmpty() == true)
-	  cout << "Stack is empty" << endl;
+		cout << "Stack is empty" << endl;
 	else
-	  cout << "Stack is full" << endl;
-
-	return 0;	
+		cout << "Stack is full" << endl;
+	return 0;
 }
-

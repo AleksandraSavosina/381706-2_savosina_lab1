f0a5b486b8be0ee64ea92a61c7df99b6abc12589
AleksandraSavosina <38883034+AleksandraSavosina@users.noreply.github.com>
2019-02-10 20:56:46 +0300

    Update Queue.h

--- queue/Queue.h
+++ queue/Queue.h
@@ -1,58 +1,33 @@
 template <class T>
 
 class TQueue
-
 {
-
 protected:
-
-	int size; // размер стека
-
-	T* mas; // элементы стека
-
-	int top; // верх
-
-	int start; // начало очереди
-
-	int end; // конец очереди
-
+  int size; // размер стека
+  T* mas; // элементы стека
+  int top; // верх
+  int start; // начало очереди
+  int end; // конец очереди
 public:
+  TQueue() // конструктор по умолчанию
+  {
+    size = 0;
+    mas = 0;
+    top = 0;
+    start = 0;
+    end = 0;
+  }
 
-	TQueue() // конструктор по умолчанию
-
-	{
-
-		size = 0;
-
-		mas = 0;
-
-		top = 0;
-
-		start = 0;
-
-		end = 0;
-
-	}
-
-	TQueue(int _size) // конструктор с параметром
-
-	{
-
-		if (_size < 0)
-
-			throw "negative size";
-
-		mas = new T[_size];
-
-		size = _size;
-
-		top = 0;
-
-		start = 0;
-
-		end = 0;
-
-	}
+  TQueue(int _size) // конструктор с параметром
+  {
+    if (_size < 0)
+      throw "negative size";
+    mas = new T[_size];
+    size = _size;
+    top = 0;
+    start = 0;
+    end = 0;
+  }
 
 	// TQueue(TQueue &h) // конструктор копирования
 
@@ -66,66 +41,38 @@ public:
 
 	// }
 
-	void Put(T h) // положить в конец очереди
+  void Put(T h) // положить в конец очереди
+  {
+    if (IsFulll())
+      throw "error: queue is full";
+    else
+    {
+      mas[start] = h;
+      start = (start + 1) % size;
+      end++;
+    }
+  }
 
-	{
+  T Get() // взять первый элемент
+  {
+    if (IsEmptyy())
+      throw "error: queue is empty";
+    else
+    {
+      T tmp = mas[top];
+      top = (top + 1) % size;
+      end--;
+      return tmp;
+    }
+  }
 
-		if (IsFulll())
+  bool IsFulll() // проверка на полноту
+  {
+    return (end == size);
+  }
 
-			throw "error: queue is full";
-
-		else
-
-		{
-
-			mas[start] = h;
-
-			start = (start + 1) % size;
-
-			end++;
-
-		}
-
-	}
-
-	T Get() // взять первый элемент
-
-	{
-
-		if (IsEmptyy())
-
-			throw "error: queue is empty";
-
-		else
-
-		{
-
-			T tmp = mas[top];
-
-			top = (top + 1) % size;
-
-			end--;
-
-			return tmp;
-
-		}
-
-	}
-
-	bool IsFulll() // проверка на полноту
-
-	{
-
-		return (end == size);
-
-	}
-
-	bool IsEmptyy() // проверка на пустоту
-
-	{
-
-		return (end == 0);
-
-	}
-
+  bool IsEmptyy() // проверка на пустоту
+  {
+    return (end == 0);
+  }
 };

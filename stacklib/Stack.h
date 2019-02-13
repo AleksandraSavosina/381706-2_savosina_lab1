1a0e20a34f9af046914e0d2c21a992e29e7df1f7
Alexandra Savosina <alexandra.savosina@gmail.com>
2019-02-13 15:13:53 +0300

    my work

--- stacklib/Stack.h
+++ stacklib/Stack.h
@@ -1,26 +1,25 @@
-
 #ifndef __STACK_H__
 #define __STACK_H__
-
 #include <iostream>
 
 using namespace std;
 
 template <class T>
+
 class TStack
 {
 protected:
-	int size;		// размер стека
-	T* mas;			// элементы стека
-	int top;		// верх
+	int size; // размер стека
+	T* mas; // элементы стека
+	int top; // верх
 public:
-	TStack()				// конструктор по умолчанию
+	TStack() // конструктор по умолчанию
 	{
 		size = 0;
 		mas = 0;
 		top = 0;
 	}
-	TStack(int _size)		// конструктор с параметром
+	TStack(int _size) // конструктор с параметром
 	{
 		if (_size < 0)
 			throw "negative size";
@@ -31,19 +30,23 @@ public:
 			mas = 0;
 			top = 0;
 		}
+
 		else
 		{
+
 			mas = new T[_size];
 			size = _size;
 			top = 0;
 		}
 	}
-	TStack(TStack &h)		// конструктор копирования
+
+	TStack(TStack &h) // конструктор копирования
 	{
 		size = h.size;
 		top = h.top;
 		if (size == 0)
 			mas = 0;
+
 		else
 		{
 			mas = new T[size];
@@ -52,39 +55,46 @@ public:
 		}
 	}
 
-	~TStack()				// деструктор
+	~TStack() // деструктор
 	{
 		delete[] mas;
 	}
 
-	void Put(T h)			// положить
+	void Put(T h) // положить
 	{
 		if (IsFull())
 			throw "stack is full";
+
 		else
 		{
 			mas[top] = h;
 			top++;
 		}
 	}
-	T Get()				// взять
+
+	T Get() // взять
 	{
+
 		if (IsEmpty())
 			throw "stack is empty";
 		else
 		{
-			top--;
+			--top;
 			return mas[top];
 		}
+
 	}
-	bool IsFull()			// проверка на полноту
+
+	bool IsFull() // проверка на полноту
 	{
 		return (top >= size);
 	}
-	bool IsEmpty()			// проверка на пустоту
+
+	bool IsEmpty() // проверка на пустоту
 	{
 		return (top == 0);
 	}
+
 };
 
 #endif
